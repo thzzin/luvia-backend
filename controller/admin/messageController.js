aed7492798624778fb723ecdae1176002d920014
thzzin <thithipaiva@hotmail.com>
2024-09-23 23:57:27 -0300

    ashdi

--- controller/admin/messageController.js
+++ controller/admin/messageController.js
@@ -1,121 +1,122 @@
 const {
-    findContactByPhoneNumber,
-    findConversationByContactId,
-    receivedMessage,
-    botMsg,
-    msgClient,
-    postImg,
-    postAudios
-} = require('../../service/messageService')
+  findContactByPhoneNumber,
+  findConversationByContactId,
+  receivedMessage,
+  botMsg,
+  msgClient,
+  postImg,
+  postAudios,
+} = require("../../service/messageService");
 
 async function FindContact(phoneNumber) {
-    try {
-        const contact = await findContactByPhoneNumber(phoneNumber);
-        res.json(contact);
-      } catch (err) {
-        console.error('Error fetching users', err.stack);
-        res.status(500).send('Server error');
-      }
+  try {
+    const contact = await findContactByPhoneNumber(phoneNumber);
+    res.json(contact);
+  } catch (err) {
+    console.error("Error fetching users", err.stack);
+    res.status(500).send("Server error");
+  }
 }
 
-
 async function FindConversation(contactId) {
-    try {
-        const contact = await findConversationByContactId(contactId);
-        res.json(contact);
-      } catch (err) {
-        console.error('Error fetching users', err.stack);
-        res.status(500).send('Server error');
-      }
+  try {
+    const contact = await findConversationByContactId(contactId);
+    res.json(contact);
+  } catch (err) {
+    console.error("Error fetching users", err.stack);
+    res.status(500).send("Server error");
+  }
 }
 
 async function PostMsg(req, res) {
   const incomingData = req.body; // Assume que o body contém um array de mensagens
-  
+
   try {
     // Enviar os dados necessários para a função receivedMessage
     await receivedMessage(incomingData);
-    res.status(200).json({ message: 'Mensagens processadas com sucesso!' });
+    res.status(200).json({ message: "Mensagens processadas com sucesso!" });
   } catch (err) {
-    console.error('Error fetching users', err.stack);
-    res.status(500).send('Server error');
+    console.error("Error fetching users", err.stack);
+    res.status(500).send("Server error");
   }
 }
 
 async function UserSendMsg(req, res) {
-  const text = req.body
+  const text = req.body;
   const adminId = req.user.id;
 
-  try{
-  const msg = await msgClient(adminId, conversationId , phonecontact, idConversa, content)
-  res.json({msg})
-  }catch(error){
-    console.error('Error send msg', err.stack);
-    res.status(500).send('Server error');
+  try {
+    const msg = await msgClient(
+      adminId,
+      conversationId,
+      phonecontact,
+      idConversa,
+      content
+    );
+    res.json({ msg });
+  } catch (error) {
+    console.error("Error send msg", err.stack);
+    res.status(500).send("Server error");
   }
 }
 
 async function PostBotMsg(req, res) {
- 
   const incomingData = req.body;
-  console.log('recebeu:', incomingData)
-  try{
-    const msg = await botMsg(incomingData)
-    res.json(msg)
-  }catch(error){
-    console.log('erro ao pegar msg robo', error)
-    res.status(500).send('Server error');
+  console.log("recebeu:", incomingData);
+  try {
+    const msg = await botMsg(incomingData);
+    res.json(msg);
+  } catch (error) {
+    console.log("erro ao pegar msg robo", error);
+    res.status(500).send("Server error");
   }
-
 }
 
 async function PostBotImg(req, res) {
   const incomingData = req.body;
-  console.log('recebeu:', incomingData)
-  try{
-    const msgImg = await postImg(incomingData)
-    res.json(msgImg)
-  }catch(error){
-    console.log('erro ao pegar msg robo', error)
-    res.status(500).send('Server error');
+  console.log("recebeu img:", incomingData);
+  try {
+    const msgImg = await postImg(incomingData);
+    res.json(msgImg);
+  } catch (error) {
+    console.log("erro ao pegar msg robo", error);
+    res.status(500).send("Server error");
   }
 }
 
 async function PostAudio(req, res) {
   const incomingData = req.body;
-  console.log('recebeu:', incomingData)
+  console.log("recebeu:", incomingData);
 
-  try{
-    const msgAudio = await postAudios(incomingData)
-    res.json(msgAudio)
-  }catch(error){
-    console.log('erro ao pegar msg robo', error)
-    res.status(500).send('Server error');
-
+  try {
+    const msgAudio = await postAudios(incomingData);
+    res.json(msgAudio);
+  } catch (error) {
+    console.log("erro ao pegar msg robo", error);
+    res.status(500).send("Server error");
   }
 }
 
 async function PostDoc(req, res) {
   const incomingData = req.body;
-  console.log('recebeu:', incomingData)
+  console.log("recebeu:", incomingData);
 
-  try{
-    const msgDoc = await postDoc(incomingData)
-    res.json(msgDoc)
-  }catch(error){
-    console.log('erro ao pegar msg robo', error)
-    res.status(500).send('Server error');
-
+  try {
+    const msgDoc = await postDoc(incomingData);
+    res.json(msgDoc);
+  } catch (error) {
+    console.log("erro ao pegar msg robo", error);
+    res.status(500).send("Server error");
   }
 }
 
 module.exports = {
-    FindContact,
-    FindConversation,
-    PostMsg,
-    UserSendMsg,
-    PostBotMsg,
-    PostBotImg,
-    PostAudio,
-    PostDoc
-}
+  FindContact,
+  FindConversation,
+  PostMsg,
+  UserSendMsg,
+  PostBotMsg,
+  PostBotImg,
+  PostAudio,
+  PostDoc,
+};
--- index.js
+++ index.js
@@ -1,48 +1,57 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // Importa o body-parser
-const pool = require('./config/db');
-require('dotenv').config();
-const cors = require('cors');
-const Message = require('./models/Message'); // Certifique-se de importar o modelo correto
-const sequelize = require('./config/db');
-const compression = require('compression'); // Importa o pacote compression
+const express = require("express");
+const bodyParser = require("body-parser"); // Importa o body-parser
+const pool = require("./config/db");
+require("dotenv").config();
+const cors = require("cors");
+const Message = require("./models/Message"); // Certifique-se de importar o modelo correto
+const sequelize = require("./config/db");
+const compression = require("compression"); // Importa o pacote compression
 
-const https = require('https'); // Importa o módulo https
-const fs = require('fs'); // Importa o módulo fs para lidar com arquivos
+const https = require("https"); // Importa o módulo https
+const fs = require("fs"); // Importa o módulo fs para lidar com arquivos
 
 // Carregando os certificados SSL
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/getluvia.com.br/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/getluvia.com.br/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/getluvia.com.br/chain.pem', 'utf8');
 
-let credentials
+let credentials;
 
 const app = express();
 const port = process.env.PORT || 3005; // Porta do backend (3005)
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const coiso = process.env.PRODUCAO
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const coiso = process.env.PRODUCAO;
+console.log("coiso: ", coiso);
 let corsOptions;
 
-if(!coiso){
-   corsOptions = {
+if (!coiso) {
+  corsOptions = {
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: false,
     optionsSuccessStatus: 204,
   };
-
-  
-  
 } else {
+  const privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/getluvia.com.br/privkey.pem",
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/getluvia.com.br/cert.pem",
+    "utf8"
+  );
+  const ca = fs.readFileSync(
+    "/etc/letsencrypt/live/getluvia.com.br/chain.pem",
+    "utf8"
+  );
+
   credentials = {
     key: privateKey,
     cert: certificate,
-    ca: ca
+    ca: ca,
   };
-  
-   corsOptions = {
+
+  corsOptions = {
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
@@ -60,10 +69,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // Middleware para parsing d
 // Função para verificar a conexão com o banco de dados
 async function checkDatabaseConnection() {
   try {
-    await pool.query('SELECT NOW()');
-    console.log('Database connection successful!');
+    await pool.query("SELECT NOW()");
+    console.log("Database connection successful!");
   } catch (err) {
-    console.error('Database connection error:', err.stack);
+    console.error("Database connection error:", err.stack);
     process.exit(1);
   }
 }
@@ -72,30 +81,29 @@ async function syncDatabase() {
   try {
     // Sincroniza todos os modelos que ainda não foram criados no banco de dados
     await sequelize.sync({ force: false }); // force: false não apaga dados existentes
-    console.log('Tabelas sincronizadas com sucesso!');
+    console.log("Tabelas sincronizadas com sucesso!");
   } catch (error) {
-    console.error('Erro ao sincronizar o banco de dados:', error);
+    console.error("Erro ao sincronizar o banco de dados:", error);
   }
 }
 
 syncDatabase();
 
 // Definição das rotas utilizando os controladores
-app.get('/', (req, res) => res.send('Vai Corinthians! foi'));
-app.use('/auth', authRoutes); 
-app.use('/user', userRoutes); 
+app.get("/", (req, res) => res.send("Vai Corinthians! foi"));
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
 // Inicia o servidor HTTPS após verificar a conexão com o banco de dados
 checkDatabaseConnection().then(() => {
-  if(!coiso){
-    return app.listen(port, () => {
+  if (!coiso) {
+    app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
-    
+  } else {
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port, () => {
+      console.log(`Servidor HTTPS rodando na porta https://localhost:${port}`);
+    });
   }
-  
-  const httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(port, () => {
-    console.log(`Servidor HTTPS rodando na porta https://localhost:${port}`);
-  });
 });
