5a7366e2c0f901c7313e3f712cb7a6e1c4f89ed9
thzzin <thithipaiva@hotmail.com>
2024-09-30 17:27:33 -0300

    debug funcao post img

--- controller/admin/messageController.js
+++ controller/admin/messageController.js
@@ -74,7 +74,7 @@ async function PostBotMsg(req, res) {
 
 async function PostBotImg(req, res) {
   const incomingData = req.body;
-
+  console.log("caiu no post img, incomingdata:", incomingData);
   try {
     const msgImg = await postImg(incomingData);
     res.json(msgImg);
