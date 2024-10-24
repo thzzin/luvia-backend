d93ec8e41aca52c685942435b7dab195000a9fc3
thzzin <thithipaiva@hotmail.com>
2024-10-23 23:54:11 -0300

    teste

--- service/bot.js
+++ service/bot.js
@@ -217,11 +217,9 @@ async function handleMessage(userMessage, cliente) {
           // Formatar as linhas encontradas no PDF
           const modelosFormatados = linhasDoPDF
             .map((linha) => {
-              // Supondo que o preço esteja no formato R$ ou outro padrão monetário
-              const precoRegex = /(R\$[0-9,.]+)/;
+              const precoRegex = /(r\$[0-9,.]+)/;
               const precoEncontrado = linha.match(precoRegex);
 
-              // Remover prefixos como "f." e manter descrição e preço
               const descricao = linha.replace("f.", "").trim();
               const preco = precoEncontrado
                 ? precoEncontrado[0]
@@ -231,10 +229,18 @@ async function handleMessage(userMessage, cliente) {
             })
             .join("\n");
 
-          // Nova mensagem incluindo as descrições e preços encontrados no PDF
+          // Verificar se alguma linha do PDF não estava na resposta do ChatGPT
+          const linhasChatGPT = response
+            .toLowerCase()
+            .split("\n")
+            .map((linha) => linha.trim());
+          const linhasFaltantes = modelosFormatados
+            .split("\n")
+            .filter((linha) => !linhasChatGPT.includes(linha));
+
           const novaResposta = `
-A tela disponível para o modelo ${modelo} na loja é a seguinte:\n${modelosFormatados}\n\n${response}
-`;
+            A tela disponível para o modelo ${modelo} na loja é a seguinte:\n${modelosFormatados}\n\n${response}
+        `;
 
           if (linhasFaltantes.length > 0) {
             console.log(
